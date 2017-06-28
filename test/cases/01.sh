b185600b1208524987f1215e65f7a668a79dc774
OrKoN <alexei.rudenko@gmail.com>
2017-06-28 23:37:28 +0200

    First test case

---
+++ test/cases/01.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+var exec = require('../..');
+
+exec("env", [], {
+  fake: 'env',
+  any: 'env',
+  dißßerent: 'chäräcters',
+  різні: "символи",
+  аЯкщо: "так=ось",
+  number: 10,
+  boolean: 1,
+  nil: null,
+  undefined: undefined,
+  etc: undefined,
+});
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,7 +1,23 @@
-require('..')("env", [], {
-  fake: 'env',
-  any: 'env',
-  dißßerent: 'chäräcters',
-  різні: "символи",
-  аЯкщо: "так=ось"
+var child_process = require('child_process');
+var assert = require('assert');
+
+describe('native-exec', function() {
+  describe('01.sh', function() {
+    it('should set environmental variables', function() {
+      var result = child_process.spawnSync('./test/cases/01.sh').stdout.toString('utf-8');
+      assert.strictEqual(
+        result,
+        'fake=env\n' + 
+        'any=env\n' + 
+        'dißßerent=chäräcters\n' + 
+        'різні=символи\n' + 
+        'аЯкщо=так=ось\n' + 
+        'number=10\n' + 
+        'boolean=1\n' + 
+        'nil=null\n' + 
+        'undefined=undefined\n' + 
+        'etc=undefined\n'
+      );
+    });
+  });
 });
